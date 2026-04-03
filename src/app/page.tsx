aea014262707b3cbc6b70e6138aecd6f710b70b2
linsgooudd-gif <linsgooudd@gmail.com>
2026-04-03 15:28:11 -0400

    Add Home component with layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,3 +2,16 @@ import Header from "../components/Header";
 import GameSection from "../components/GameSection";
 import BlogSection from "../components/BlogSection";
 import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex-1 flex flex-col items-center w-full">
+        <GameSection />
+        <BlogSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
