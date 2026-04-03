862f562401590dfdfe03a7ec8bcafcdb3a05fcdb
linsgooudd-gif <linsgooudd@gmail.com>
2026-04-03 15:08:46 -0400

    Change import paths for components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
-import Header from "@/components/Header";
-import GameSection from "@/components/GameSection";
-import BlogSection from "@/components/BlogSection";
-import Footer from "@/components/Footer";
-
-export default function Home() {
-  return (
-    <>
-      <Header />
-      <main className="flex-1 flex flex-col items-center w-full">
-        <GameSection />
-        <BlogSection />
-      </main>
-      <Footer />
-    </>
-  );
-}
+import Header from "../components/Header";
+import GameSection from "../components/GameSection";
+import BlogSection from "../components/BlogSection";
+import Footer from "../components/Footer";
